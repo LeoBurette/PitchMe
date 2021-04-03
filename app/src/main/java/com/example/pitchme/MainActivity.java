af27c5d469c466f109acfa8ae4aaecf99e47c55e
leoBurette <leo.burette@etu.unice.fr>
2021-04-03 11:55:15 +0200

    graph

--- app/src/main/java/com/example/pitchme/MainActivity.java
+++ app/src/main/java/com/example/pitchme/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.pitchme.word.DataSingleton;
@@ -28,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent addMovie = new Intent(MainActivity.this, MovieGestionActivity.class);
                 startActivity(addMovie);
+            }
+        });
+        (findViewById(R.id.buttonInformation)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent information = new Intent(MainActivity.this, InformationActivity.class);
+                startActivity(information);
+
             }
         });
     }
